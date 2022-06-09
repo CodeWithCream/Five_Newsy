aec629a05a342c4fe8e1f68898836dd0482daea6
Andrea Knez Karačić <zelenokosazelena@gmail.com>
2022-06-09 20:48:43 +0200

    feat: header

--- five_newsy_web/src/app/app.module.ts
+++ five_newsy_web/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleListComponent } from './components/article-list/article-list.component';
 import { ArticleComponent } from './components/article/article.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleListComponent,
-    ArticleComponent
+    ArticleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
