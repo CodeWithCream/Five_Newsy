063ac0e8569ad4fbb2b4e7af9e943e3da6ee03b9
Andrea Knez Karačić <zelenokosazelena@gmail.com>
2022-06-09 19:48:02 +0200

    feat: add empty article-list component

--- five_newsy_web/src/app/app-routing.module.ts
+++ five_newsy_web/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ArticleListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
